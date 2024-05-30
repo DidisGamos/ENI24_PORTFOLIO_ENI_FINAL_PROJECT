42aaced30ccbb83c89b1f9a52d9b7cab22e900dc
DidisGamos <didisgamos@gmail.com>
2024-05-30 14:27:30 +0300

    changes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
+let lastScrollTop = 0;
 window.addEventListener('scroll', function() {
     var header = document.getElementById('header');
-    if (window.scrollY > 50) {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // Downscroll
         header.style.transform = 'translateY(-100%)';
     } else {
+        // Upscroll
         header.style.transform = 'translateY(0)';
     }
+    lastScrollTop = scrollTop;
 });
